ee94c47cd03b746d264b8cb1bc460f30c443b4e0
schoolheilok <schoolheilok@gmail.com>
2025-08-29 16:11:58 -0400

    Create Task1.py
    created - first commit

---
+++ lab1/Task1.py
@@ -0,0 +1,12 @@
+/*
+## Question 1: Statements and Expressions (10 points)  
+- Write three lines of Python code that include:  
+  - An assignment statement assigning a value to a variable  
+  - A print statement displaying a message  
+  - An expression involving arithmetic operations  
+- Add comments explaining which line is a statement and which is an expression.
+*/
+
+temperature = 25 # Assignment statement  
+today = temperature + 10  # temperature + 10 is an expression that evaluates to 35 if x is 25
+print("Hi welcome, today's temperature is ",today,"degrees celsius")
